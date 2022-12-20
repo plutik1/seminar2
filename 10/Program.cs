08ae7b088812ab9f4a15a0f57a9ed7918c0e2869
plutik <you@exm>
2022-12-20 22:59:42 +0300

    add zadanie 10

---
+++ 10/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+Console.WriteLine("Введите трехзначное число.");
+double input = Convert.ToDouble(Console.ReadLine());
+        
+
+    double s =input%100  ;
+
+    input = s / 10;
+
+    int q = Convert.ToInt32(input);
+    
+Console.WriteLine(q);  
+  

5d96a388f9b21fe600b94d19d7b57980ed9ee9f1
plutik <you@exm>
2022-12-21 22:38:20 +0300

    добовили задание 15

---
+++ 15/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+Console.WriteLine("введите день недели 1-понедльник....7-воскресенье"); 
+int input = Convert.ToInt32(Console.ReadLine());
+int o=0;
+
+int[] arr= {1,2,3,4,5,6,7};
+//Console.WriteLine(arr[3]); 
+
+for (int i=0; i<6;i++)// заготовка на ввод\вывод массива
+    {
+         
+    }   
+
+    if(input==6 || input ==7)
+        {
+            Console.WriteLine("да"); 
+        }
+
+        if(input==1 || input ==2 || input==3 || input ==4 || input ==5)
+        {
+            Console.WriteLine("нет"); 
+        }

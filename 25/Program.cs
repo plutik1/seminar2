820c5da5cb3e7cd97d01eba04f6cbd8b2a324152
plutik <you@exm>
2023-01-18 19:26:45 +0300

    25 add

---
+++ 25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("a"); 
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b"); 
+int b = Convert.ToInt32(Console.ReadLine());
+
+int summ=1;
+for (int i=0; i<=b; i++)
+summ=summ*a;
+Console.WriteLine(summ); 

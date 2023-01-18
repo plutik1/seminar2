0cc73a69b151f2034c2f873b5a8418439048721a
plutik <you@exm>
2023-01-18 20:26:09 +0300

    29 add

---
+++ 29/Program.cs
@@ -0,0 +1,19 @@
+﻿int lent = 8;
+int[] a = new int[lent];
+Random rnd = new Random();
+Console.Write("[");
+for (int i =0; i<lent; i++)
+{
+    a[i]=rnd.Next();
+    
+    if (i==lent-1)
+    {
+        Console.Write(a[i]);
+    }
+    else
+    {
+        Console.Write(a[i] + ", ");
+    }
+   // Console.Write(", ");
+}
+Console.Write("]");
